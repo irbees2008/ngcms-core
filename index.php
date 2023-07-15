5cbffb3a55e117f8df7098a4ff20da63caa01bdd
Станислав Гольцов <irbees2008@gmail.com>
2023-07-15 15:55:29 +0600

    Update index.php

--- index.php
+++ index.php
@@ -7,324 +7,238 @@
 // Author: NGCMS project team
 //
 
-<?php
-
-// Include core module at the beginning
-include_once 'engine/core.php';
-
 // Override charset
-@header('Content-Type: text/html; charset=utf-8');
+@header('content-type: text/html; charset=utf-8');
 
 // Check for minimum supported PHP version
 if (version_compare(PHP_VERSION, '7.2.0') < 0) {
-    exitWithError('NGCMS required PHP version 7.2+ / Необходима версия PHP 7.2 или выше');
+    @header('content-type: text/html; charset=utf-8');
+    echo "<html><head><title>NGCMS required PHP version 7.2+ / Необходима версия PHP 7.2 или выше</title></head><body><div style='font: 24px verdana; background-color: #EEEEEE; border: #ABCDEF 1px solid; margin: 1px; padding: 3px;'><span style='color: red;'>FATAL ERROR / Фатальная ошибка</span><br/><br/><span style=\"font: 16px arial;\"> NGCMS requires PHP version <b>7.2+</b><br/>Please ask your hosting provider to upgrade your account</span><br/><hr/><span style=\"font: 16px arial;\"> Для работы NGCMS требуется PHP версии <b>7.2</b> или выше.<br/>Обратитесь к вашему хостинг провайдеру для обновления версии</span></div></body></html>";
+    exit;
 }
 
-// Initialize GZip handler
-initGZipHandler();
-
-// Load configuration and user data
-$config = getConfig();
-$userROW = getUserData();
-$systemAccessURL = getSystemAccessURL();
-
-// Check if site access is locked [ for everyone except admins ]
-if (isSiteAccessLocked($config, $userROW)) {
-    displaySiteLockPage($config);
-}
-
-// Start generating page
-executeActionHandler('index_pre');
-
-// Generate fatal 404 error [NOT FOUND] if URL handler didn't find any task for execution
-if (!executeURLHandler($systemAccessURL)) {
-    error404();
-}
-
-executeActionHandler('index');
-
-// Generate additional informational blocks
-$template = generateTemplateData();
-$template['vars']['categories'] = generateCategoryMenu();
-$template['vars']['titles'] = implode(' : ', array_values($SYSTEM_FLAGS['info']['title']));
-$template['vars']['category'] = isset($_REQUEST['category']) && ($_REQUEST['category'] != '') ? secure_html($_REQUEST['category']) : '';
-
-executeActionHandler('index_post');
-
-// Prepare JS/CSS/RSS references
-$template['vars']['htmlvars'] = generateHTMLVars();
-
-// Add support of blocks [is-logged] .. [/isnt-logged] in main template
-$template = applyUserBlocks($template, $userROW);
-
-// Calculate script execution time
-$template['vars']['queries'] = getDatabaseQueryCount();
-$template['vars']['exectime'] = getExecutionTime();
-
-// Fill debug information (if it is requested)
-if ($config['debug']) {
-    $template = generateDebugInfo($template, $config, $userROW);
-}
-
-// Generate template for main page
-$mainTemplateName = getMainTemplateName();
-$mainTemplatePath = getMainTemplatePath();
-$output = generateTemplateOutput($mainTemplateName, $mainTemplatePath, $template);
-
-// Print the final output
-printHTTPHeaders();
-echo $output;
-
-// Maintanance activities
-session_write_close();
-runCronTasks();
-coreNormalTerminate();
-
-// Helper functions
-
-function exitWithError($errorMessage) {
-    exit("<html><head><title>Error</title></head><body><div>$errorMessage</div></body></html>");
-}
-
-function isSiteAccessLocked($config, $userROW) {
-    return $config['lock'] && (!is_array($userROW) || (!checkPermission(['plugin' => '#admin', 'item' => 'system'], null, 'lockedsite.view')));
-}
-
-function displaySiteLockPage($config) {
-    if (file_exists(tpl_site.'sitelock.tpl')) {
-        $tVars = ['lock_reason' => $config['lock_reason']];
-        $xt = $twig->loadTemplate('sitelock.tpl');
-        echo $xt->render($tVars);
-    } else {
-        exitWithError('Site is disabled with reason: '.$config['lock_reason']);
-    }
-}
-
-function executeURLHandler($systemAccessURL) {
-    global $UHANDLER;
-    <?php
-
-// Include core module at the beginning
+// Load CORE module
 include_once 'engine/core.php';
 
-// Override charset
-@header('Content-Type: text/html; charset=utf-8');
+/**
+ * @var $config
+ * @var $userROW
+ * @var $twig
+ * @var $timer
+ * @var $systemAccessURL
+ * @var $UHANDLER
+ * @var $EXTRA_CSS
+ * @var $mysql
+ * @var $tpl
+ * @var $SUPRESS_TEMPLATE_SHOW
+ * @var $SUPRESS_MAINBLOCK_SHOW
+ * @var $cron
+ */
 
-// Check for minimum supported PHP version
-if (version_compare(PHP_VERSION, '7.2.0') < 0) {
-    exitWithError('NGCMS required PHP version 7.2+ / Необходима версия PHP 7.2 или выше');
-}
-
-// Initialize GZip handler
+// Init GZip handler
 initGZipHandler();
 
-// Load configuration and user data
-$config = getConfig();
-$userROW = getUserData();
-$systemAccessURL = getSystemAccessURL();
+// Define default TITLE
+$SYSTEM_FLAGS['info']['title'] = [];
+$SYSTEM_FLAGS['info']['title']['header'] = home_title;
 
+// Initialize main template array
+$template = [
+    'vars' => [
+        'what'       => engineName,
+        'version'    => engineVersion,
+        'home'       => home,
+        'titles'     => home_title,
+        'home_title' => home_title,
+        'mainblock'  => '',
+        'htmlvars'   => '',
+    ],
+];
+
+// ===================================================================
 // Check if site access is locked [ for everyone except admins ]
-if (isSiteAccessLocked($config, $userROW)) {
-    displaySiteLockPage($config);
+// ===================================================================
+if ($config['lock'] && (!is_array($userROW) || (!checkPermission(['plugin' => '#admin', 'item' => 'system'], null, 'lockedsite.view')))) {
+    // Generate sitelock.tpl instead of content page
+    if (!file_exists(tpl_site.'sitelock.tpl')) {
+        echo 'Site is disabled with reason: '.$config['lock_reason'];
+    } else {
+        $tVars = $template['vars'];
+        $tVars['lock_reason'] = $config['lock_reason'];
+
+        $xt = $twig->loadTemplate('sitelock.tpl');
+        echo $xt->render($tVars);
+    }
+
+    // STOP SCRIPT EXECUTION
+    exit;
 }
 
+// ===================================================================
 // Start generating page
+// ===================================================================
+
+// External call: before executing URL handler
 executeActionHandler('index_pre');
 
-// Generate fatal 404 error [NOT FOUND] if URL handler didn't find any task for execution
-if (!executeURLHandler($systemAccessURL)) {
+// Deactivate block [sitelock] ... [/sitelock]
+$template['vars']['[sitelock]'] = '';
+$template['vars']['[/sitelock]'] = '';
+
+// /////////////////////////////////////////////////////////// //
+// You may modify variable $systemAccessURL here (for hacks)   //
+// /////////////////////////////////////////////////////////// //
+
+// /////////////////////////////////////////////////////////// //
+$timer->registerEvent('Search route for URL "'.$systemAccessURL.'"');
+
+// Give domainName to URL handler engine for generating absolute links
+$UHANDLER->setOptions(['domainPrefix' => $config['home_url']]);
+
+// Check if engine is installed in subdirectory
+if (preg_match('#^http\:\/\/([^\/])+(\/.+)#', $config['home_url'], $match)) {
+    $UHANDLER->setOptions(['localPrefix' => $match[2]]);
+}
+$runResult = $UHANDLER->run($systemAccessURL, ['debug' => false]);
+
+// [[MARKER]] URL handler execution is finished
+$timer->registerEvent('URL handler execution is finished');
+
+// Generate fatal 404 error [NOT FOUND] if URL handler didn't found any task for execution
+if (!$runResult) {
     error404();
 }
 
+// External call: after executing URL handler
 executeActionHandler('index');
 
+// ===================================================================
 // Generate additional informational blocks
-$template = generateTemplateData();
-$template['vars']['categories'] = generateCategoryMenu();
-$template['vars']['titles'] = implode(' : ', array_values($SYSTEM_FLAGS['info']['title']));
-$template['vars']['category'] = isset($_REQUEST['category']) && ($_REQUEST['category'] != '') ? secure_html($_REQUEST['category']) : '';
+// ===================================================================
+$timer->registerEvent('General plugins execution is finished');
 
+// Generate category menu
+$template['vars']['categories'] = generateCategoryMenu();
+$timer->registerEvent('Category menu created');
+
+// Generate page title
+$template['vars']['titles'] = implode(' : ', array_values($SYSTEM_FLAGS['info']['title']));
+
+// Generate user menu
+coreUserMenu();
+
+// Generate search form
+coreSearchForm();
+
+// Save 'category' variable
+$template['vars']['category'] = (isset($_REQUEST['category']) && ($_REQUEST['category'] != '')) ? secure_html($_REQUEST['category']) : '';
+
+// ====================================================================
+// External call: All variables for main template are generated
+// ===================================================================
 executeActionHandler('index_post');
 
+// ===================================================================
 // Prepare JS/CSS/RSS references
-$template['vars']['htmlvars'] = generateHTMLVars();
+
+// Make empty OLD STYLE variables
+$template['vars']['metatags'] = '';
+$template['vars']['extracss'] = '';
+
+// Fill extra CSS links
+foreach ($EXTRA_CSS as $css => $null) {
+    $EXTRA_HTML_VARS[] = ['type' => 'css', 'data' => $css];
+}
+
+// Generate metatags
+$EXTRA_HTML_VARS[] = ['type' => 'plain', 'data' => GetMetatags()];
+
+// Fill additional HTML vars
+$htmlrow = [];
+$dupCheck = [];
+foreach ($EXTRA_HTML_VARS as $htmlvar) {
+    // Skip empty
+    if (!$htmlvar['data']) {
+        continue;
+    }
+
+    // Check for duplicated rows
+    if (in_array($htmlvar['data'], $dupCheck)) {
+        continue;
+    }
+    $dupCheck[] = $htmlvar['data'];
+
+    switch ($htmlvar['type']) {
+        case 'css':
+            $htmlrow[] = '<link href="'.$htmlvar['data'].'" rel="stylesheet" type="text/css" />';
+            break;
+        case 'js':
+            $htmlrow[] = '<script type="text/javascript" src="'.$htmlvar['data'].'"></script>';
+            break;
+        case 'rss':
+            $htmlrow[] = '<link href="'.$htmlvar['data'].'" rel="alternate" type="application/rss+xml" title="RSS" />';
+            break;
+        case 'plain':
+            $htmlrow[] = $htmlvar['data'];
+            break;
+    }
+}
+if (count($htmlrow)) {
+    $template['vars']['htmlvars'] .= implode("\n", $htmlrow);
+}
 
 // Add support of blocks [is-logged] .. [/isnt-logged] in main template
-$template = applyUserBlocks($template, $userROW);
+$template['regx']['#\[is-logged\](.+?)\[/is-logged\]#is'] = is_array($userROW) ? '$1' : '';
+$template['regx']['#\[isnt-logged\](.+?)\[/isnt-logged\]#is'] = is_array($userROW) ? '' : '$1';
 
+// ***** EXECUTION TIME CATCH POINT *****
 // Calculate script execution time
-$template['vars']['queries'] = getDatabaseQueryCount();
-$template['vars']['exectime'] = getExecutionTime();
+$template['vars']['queries'] = $mysql->qcnt();
+$template['vars']['exectime'] = $timer->stop();
 
 // Fill debug information (if it is requested)
 if ($config['debug']) {
-    $template = generateDebugInfo($template, $config, $userROW);
-}
-
-// Generate template for main page
-$mainTemplateName = getMainTemplateName();
-$mainTemplatePath = getMainTemplatePath();
-$output = generateTemplateOutput($mainTemplateName, $mainTemplatePath, $template);
-
-// Print the final output
-printHTTPHeaders();
-echo $output;
-
-// Maintanance activities
-session_write_close();
-runCronTasks();
-coreNormalTerminate();
-
-// Helper functions
-
-function exitWithError($errorMessage) {
-    exit("<html><head><title>Error</title></head><body><div>$errorMessage</div></body></html>");
-}
-
-function isSiteAccessLocked($config, $userROW) {
-    return $config['lock'] && (!is_array($userROW) || (!checkPermission(['plugin' => '#admin', 'item' => 'system'], null, 'lockedsite.view')));
-}
-
-function displaySiteLockPage($config) {
-    if (file_exists(tpl_site.'sitelock.tpl')) {
-        $tVars = ['lock_reason' => $config['lock_reason']];
-        $xt = $twig->loadTemplate('sitelock.tpl');
-        echo $xt->render($tVars);
-    } else {
-        exitWithError('Site is disabled with reason: '.$config['lock_reason']);
-    }
-}
-
-function executeURLHandler($systemAccessURL) {
-    global $UHANDLER;
-    $UHANDLER->setOptions(['domainPrefix' => $config['home_url']]);
-    if (preg_match('#^http\:\/\/([^\/])+(\/.+)#', $config['home_url'], $match)) {
-        $UHANDLER->setOptions(['localPrefix' => $match[2]]);
-    }
-    return $UHANDLER->run($systemAccessURL, ['debug' => false]);
-}
-
-function generateTemplateData() {
-    global $template, $twig, $SYSTEM_FLAGS, $EXTRA_CSS, $EXTRA_HTML_VARS;
-    $template = [
-        'vars' => [
-            'what' => engineName,
-            'version' => engineVersion,
-            'home' => home,
-            'titles' => home_title,
-            'home_title' => home_title,
-            'mainblock' => '',
-            'htmlvars' => '',
-        ],
-        'regx' => [
-            '#\[is-logged\](.+?)\[/is-logged\]#is' => '',
-            '#\[isnt-logged\](.+?)\[/isnt-logged\]#is' => '',
-        ],
-    ];
-    return $template;
-}
-
-function generateCategoryMenu() {
-    // Generate category menu logic here
-}
-
-function generateHTMLVars() {
-    global $EXTRA_CSS, $EXTRA_HTML_VARS;
-    $htmlrow = [];
-    $dupCheck = [];
-    foreach ($EXTRA_HTML_VARS as $htmlvar) {
-        if (!$htmlvar['data']) {
-            continue;
-        }
-        if (in_array($htmlvar['data'], $dupCheck)) {
-            continue;
-        }
-        $dupCheck[] = $htmlvar['data'];
-        switch ($htmlvar['type']) {
-            case 'css':
-                $htmlrow[] = '<link href="'.$htmlvar['data'].'" rel="stylesheet" type="text/css" />';
-                break;
-            case 'js':
-                $htmlrow[] = '<script type="text/javascript" src="'.$htmlvar['data'].'"></script>';
-                break;
-            case 'rss':
-                $htmlrow[] = '<link href="'.$htmlvar['data'].'" rel="alternate" type="application/rss+xml" title="RSS" />';
-                break;
-            case 'plain':
-                $htmlrow[] = $htmlvar['data'];
-                break;
-        }
-    }
-    return implode("\n", $htmlrow);
-}
-
-function applyUserBlocks($template, $userROW) {
-    if (is_array($userROW)) {
-        $template['regx']['#\[is-logged\](.+?)\[/is-logged\]#is'] = '$1';
-    } else {
-        $template['regx']['#\[isnt-logged\](.+?)\[/isnt-logged\]#is'] = '$1';
-    }
-    return $template;
-}
-
-function getDatabaseQueryCount() {
-    global $mysql;
-    return $mysql->qcnt();
-}
-
-function getExecutionTime() {
-    global $timer;
-    return $timer->stop();
-}
-
-function generateDebugInfo($template, $config, $userROW) {
-    global $mysql, $timer;
     $timer->registerEvent('Templates generation time: '.$tpl->execTime.' ('.$tpl->execCount.' times called)');
     $timer->registerEvent('Generate DEBUG output');
     if (checkPermission(['plugin' => '#admin', 'item' => 'system'], $userROW, 'debug.view')) {
-        $template['vars']['debug_queries'] = $config['debug_queries'] ? ('<b><u>SQL queries:</u></b>'.implode("<br />", $mysql->debug_q).'Total: '.$mysql->qcnt()) : '';
-        $template['vars']['debug_exectime'] = '<b><u>Execution time:</u></b>'.$timer->show();
-        return $template;
+        $template['vars']['debug_queries'] = ($config['debug_queries']) ? ('<b><u>SQL queries:</u></b><br>'.implode("<br />\n", $mysql->query_list).'<br />') : '';
+        $template['vars']['debug_profiler'] = ($config['debug_profiler']) ? ('<b><u>Time profiler:</u></b>'.$timer->printEvents(1).'<br />') : '';
+        $template['vars']['[debug]'] = '';
+        $template['vars']['[/debug]'] = '';
+    } else {
+        $template['regx']["#\[debug\].*?\[/debug\]#si"] = '';
     }
-    
-    function getMainTemplateName() {
-        // Logic to determine the main template name
-    }
-    
-    function getMainTemplatePath() {
-        // Logic to determine the main template path
-    }
-    
-    function generateTemplateOutput($mainTemplateName, $mainTemplatePath, $template) {
-        global $tpl;
-        $tpl->loadTemplate($mainTemplateName, $mainTemplatePath);
-        $output = $tpl->render($template['vars']);
-        $output = preg_replace(array_keys($template['regx']), array_values($template['regx']), $output);
-        return $output;
-    }
-    
-    function printHTTPHeaders() {
-        header('Content-type: text/html; charset=utf-8');
-        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-        header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-        header('Cache-Control: no-store, no-cache, must-revalidate');
-        header('Cache-Control: post-check=0, pre-check=0', false);
-        header('Pragma: no-cache');
-    }
-    
-    function runCronTasks() {
-        // Logic to run cron tasks
-    }
-    
-    function coreNormalTerminate() {
-        global $timer, $mysql, $dbClose;
-        $timer->registerEvent('End of script');
-        if ($mysql) {
-            $mysql->stop();
-        }
-        if ($dbClose) {
-            mysql_close($dbClose);
-        }
-    }
-    
-    ?>
+}
+
+// ===================================================================
+// Generate template for main page
+// ===================================================================
+// 0. Calculate memory PEAK usage
+$template['vars']['memPeakUsage'] = sprintf('%7.3f', (memory_get_peak_usage() / 1024 / 1024));
+
+// 1. Determine template name & path
+$mainTemplateName = isset($SYSTEM_FLAGS['template.main.name']) ? $SYSTEM_FLAGS['template.main.name'] : 'main';
+$mainTemplatePath = isset($SYSTEM_FLAGS['template.main.path']) ? $SYSTEM_FLAGS['template.main.path'] : tpl_site;
+
+// 2. Load & show template
+$tpl->template($mainTemplateName, $mainTemplatePath);
+$tpl->vars($mainTemplateName, $template);
+if (!$SUPRESS_TEMPLATE_SHOW) {
+    printHTTPheaders();
+    echo $tpl->show($mainTemplateName);
+} elseif (!$SUPRESS_MAINBLOCK_SHOW) {
+    printHTTPheaders();
+    echo $template['vars']['mainblock'];
+}
+
+// ===================================================================
+// Maintanance activities
+// ===================================================================
+// Close opened sessions to avoid blocks
+session_write_close();
+
+// Run CRON
+$cron->run();
+
+// Terminate execution of script
+coreNormalTerminate();
